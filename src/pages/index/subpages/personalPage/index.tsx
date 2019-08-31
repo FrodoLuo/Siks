5d067f28b3bfd9bb81f1697c296494f60720e06b
wukuncai <1113765859@qq.com>
2019-08-31 13:44:51 +0800

    fix: add props

--- src/pages/index/subpages/personalPage/index.tsx
+++ src/pages/index/subpages/personalPage/index.tsx
@@ -2,20 +2,28 @@ import { Button, Form, Input, Label, View } from '@tarojs/components';
 import { inject, observer } from '@tarojs/mobx';
 import Taro from '@tarojs/taro';
 import QuestStore from 'src/store/quest';
+import AuthStore from 'src/store/auth'
 import { AtAvatar } from 'taro-ui'
 import './index.less';
 
 interface PublishPageProps {
   questStore: QuestStore;
+  authStore: AuthStore
 }
 
 @inject(store => ({
   questStore: store.questStore,
+  authStore: store.authStatus,
 }))
 @observer
 class PublishPage extends Taro.Component<PublishPageProps> {
 
   public static defaultProps: PublishPageProps;
+
+  public async componentDidMount() {
+    let res = await this.props.authStore.getUserInfo();
+    console.log(this.props.authStore.userInfo);
+  }
 
   public render() {
     return <View>
