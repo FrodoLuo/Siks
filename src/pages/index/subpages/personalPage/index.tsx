267f7d1568b735e050dc8be3c4efb75c863d856d
wukuncai <1113765859@qq.com>
2019-09-04 11:35:23 +0800

    fix: 1

--- src/pages/index/subpages/personalPage/index.tsx
+++ src/pages/index/subpages/personalPage/index.tsx
@@ -28,53 +28,53 @@ class PublishPage extends Taro.Component<PublishPageProps> {
     // console.log(userInfo);
     return <View>
       <View className="container">
-          <View className="userInfo">
-            <AtAvatar className="img" image={userInfo.icon_url} size="large" circle={true} />
-            <View className="nickname">{userInfo.nickname}</View>
-          </View>
-          <View className="buttonList">
-            <Button size="mini" type="primary" className="button" onClick={
-              () => {
-                Taro.navigateTo({
-                  url: `/pages/myTask/index?type=launch`,
-                });
-              }
-            }>我发布的</Button>
-            <Button size="mini" type="primary" className="button" onClick={
-              () => {
-                Taro.navigateTo({
-                  url: `/pages/myTask/index?type=join`,
-                });
-              }
-            }>我参与的</Button>
-          </View>
-          <View className="menu">
-            <View className="button" onClick={
-              () => Taro.showToast({
-                title: '未完待续..',
-                icon: 'none'
-              })
-            }>我的钱包</View>
-            <View className="button" onClick={
-              () => {
-                Taro.navigateTo({
-                  url: `/pages/msg-center/index`,
-                });
-              }
-            }>消息中心</View>
-            <View className="button" onClick={
-              () => Taro.showToast({
-                title: '未完待续..',
-                icon: 'none'
-              })
-            }>帮助中心</View>
-            <View className="button" onClick={
-              () => Taro.showToast({
-                title: '未完待续..',
-                icon: 'none'
-              })
-            }>投诉</View>
-          </View>
+        <View className="userInfo">
+          <AtAvatar className="img" image={userInfo.icon_url} size="large" circle={true} />
+          <View className="nickname">{userInfo.nickname}</View>
+        </View>
+        <View className="buttonList">
+          <Button size="mini" type="primary" className="button" onClick={
+            () => {
+              Taro.navigateTo({
+                url: `/pages/myTask/index?type=launch`,
+              });
+            }
+          }>我发布的</Button>
+          <Button size="mini" type="primary" className="button" onClick={
+            () => {
+              Taro.navigateTo({
+                url: `/pages/myTask/index?type=join`,
+              });
+            }
+          }>我参与的</Button>
+        </View>
+        <View className="menu">
+          <View className="button" onClick={
+            () => Taro.showToast({
+              title: '未完待续..',
+              icon: 'none'
+            })
+          }>我的钱包</View>
+          <View className="button" onClick={
+            () => {
+              Taro.navigateTo({
+                url: `/pages/msg-center/index`,
+              });
+            }
+          }>消息中心</View>
+          <View className="button" onClick={
+            () => Taro.showToast({
+              title: '未完待续..',
+              icon: 'none'
+            })
+          }>帮助中心</View>
+          <View className="button" onClick={
+            () => Taro.showToast({
+              title: '未完待续..',
+              icon: 'none'
+            })
+          }>投诉</View>
+        </View>
       </View>
     </View>;
   }
